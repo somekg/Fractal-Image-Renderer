25c5b198cf68fc4d66af4c32b5c67b27ef8c9b34
Alejandro Cabero Guerra <alej.cabero@gmail.com>
2024-10-08 19:57:19 +0200

    Cleaned FractalCreator.h

--- FractalCreator.h
+++ FractalCreator.h
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <iostream>
 #include <string>
 #include <cstdint>
@@ -11,19 +10,19 @@
 #include "Zoom.h"
 #include "ZoomList.h"
 #include "RGB.h"
-
 using namespace std;
+
 namespace me {
 class FractalCreator
 {
 private:
-	int m_width;
-	int m_height;
+	int m_width, m_height;
+	int m_total{ 0 };
+
 	unique_ptr<int[]> m_histogram;
 	unique_ptr<int[]> m_fractal;
 	Bitmap m_bitmap;
 	ZoomList m_zoomList;
-	int m_total{ 0 };
 
 	vector<int> m_ranges;
 	vector<RGB> m_colors;
@@ -31,24 +30,20 @@ private:
 
 	bool m_bGotFirstRange{ false };
 private:
-
-	
 	void calculateIteration();
 	void calculateTotalIterations();
 	void calculateRangeTotals();
 	void drawFractal();
-
 	void writeBitmap(string name);
-	
-public:int getRange(int iterations) const;
-
+public:
+	int getRange(int iterations) const;
 public:
 	FractalCreator(int width, int height);
+
 	virtual ~FractalCreator();
+
 	void addRange(double rangeEnd, const RGB& rgb);
 	void run(string name);
 	void addZoom(const Zoom& zoom);
 };
 }
-
-
