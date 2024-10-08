4ccaa1f9e5792f2ad091e82feca5f3b7f2ff15bf
Alejandro Cabero Guerra <alej.cabero@gmail.com>
2024-10-08 20:04:41 +0200

    Update ZoomList.h

--- ZoomList.h
+++ ZoomList.h
@@ -5,7 +5,6 @@
 
 using namespace std;
 namespace me {
-
 class ZoomList {
 private:
 	double m_xCenter{ 0 };
@@ -14,12 +13,11 @@ private:
 
 	int m_width{ 0 };
 	int m_height{ 0 };
+
 	vector<Zoom> zooms;
 public:
 	ZoomList(int width, int height);
 	void add(const Zoom& zoom);
 	pair<double,double> doZoom(int x, int y);
 };
-
 }
-
