27b3ebae4c8bd86d320c7d5ec627034ce66ee973
Alejandro Cabero Guerra <alej.cabero@gmail.com>
2024-10-08 19:58:49 +0200

    Cleaned Mandelbrot.h

--- Mandelbrot.h
+++ Mandelbrot.h
@@ -3,14 +3,10 @@
 namespace me {
 class Mandelbrot {
 public:
-	static const int MAX_ITERATIONS = 12000;
+	static const int MAX_ITERATIONS = 2000;
 public:
 	Mandelbrot();
 	virtual ~Mandelbrot();
-
-
 	static int getIterations(double x, double y);
 };
 }
-
-
